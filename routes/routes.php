559507ac6880537681d485c9c3e9011dbb3e9843
ria chandra mathur <rm729@afsconnect1.njit.edu>
2017-12-13 12:24:08 -0500

    added new route

--- routes/routes.php
+++ routes/routes.php
@@ -162,6 +162,14 @@ class routes
         $route->method = 'show';
         $routes[] = $route;
 
+        $route = new route();
+        $route->http_method = 'POST';
+        $route->action = 'button';
+        $route->page = 'todos';
+        $route->controller = 'tasksController';
+        $route->method = 'button';
+        $routes[] = $route;
+
 
         return $routes;
     }
