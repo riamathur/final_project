133b1de729b21d3388c86db7bc989c0457bb05c1
ria chandra mathur <rm729@afsconnect1.njit.edu>
2017-12-15 19:34:47 -0500

    modified create task page

--- pages/create_task.php
+++ pages/create_task.php
@@ -6,7 +6,9 @@
 
     Create Date: <input type="text" name="createddate" value=""><br>
     Due Date: <input type="text" name="duedate" value=""><br>
-    Message: <input type="text" name="message" value=""><br>
+    Message: <textarea rows="4" cols="50" name="message" form="usrform">
+    </textarea><br>
+
     Is Done?: <input type="text" name="isdone" value=""><br>
     <input type="submit" value="Save">
 </form>
