0ddd1c853cf93ba8cd0db0acaae9feca88a7ff28
ria chandra mathur <rm729@afsconnect1.njit.edu>
2017-12-15 19:39:17 -0500

    modified edit task page

--- pages/edit_task.php
+++ pages/edit_task.php
@@ -8,7 +8,7 @@
 
     Create Date: <input type="text" name="createddate" value="<?php echo $array['createddate']?>"><br>
     Due Date: <input type="text" name="duedate" value="<?php echo $array['duedate']?>"><br>
-    Message: <input type="text" name="message" value="<?php echo $array['message']?>"><br>
+    Message:<textarea rows="4" cols="50" name="message"  form="usrform"><?php echo $array['message']?></textarea><br>
     Is Done?: <input type="text" name="isdone" value="<?php echo $array['isdone']?>"><br>
     <input type="submit" value="Update">
 </form>
