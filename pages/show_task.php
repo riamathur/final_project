04005b7b5fb44da9814242876d7552d629e98389
ria chandra mathur <rm729@afsconnect1.njit.edu>
2017-12-17 18:22:22 -0500

    modified show task form action

--- pages/show_task.php
+++ pages/show_task.php
@@ -5,16 +5,16 @@
 <?php
 //this is how you print something  $data contains the record that was selected on the table.
 
-$array = get_object_vars($data[0]);
+$array = get_object_vars($data);
 
-//print_r($array);
+
 ?>
 
-<form action="index.php?page=todos&action=button" method="post">
+<form action="index.php?page=todos&action=button&id=<?php echo $array['id']?>" method="post">
 
     Create Date: <input type="text" name="createddate" value="<?php echo $array['createddate']?>" readonly><br>
     Due Date: <input type="text" name="duedate" value="<?php echo $array['duedate']?>" readonly><br>
-    Message:<textarea rows="4" cols="50" name="message"  form="usrform"><?php echo $array['message']?></textarea><br>
+    Message:<textarea rows="4" cols="50" name="message"  form="usrform" readonly><?php echo $array['message']?></textarea><br>
     Is Done?: <input type="text" name="isdone" value="<?php echo $array['isdone']?>" readonly><br>
     <input type="submit" name="btSubmit" value="Edit">
     <input type="submit" name="btSubmit" value="Delete">
